8c5a9f0b3997425d2d521734d2cfcc27581d8e76
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2024-10-12 19:53:44 -0300

    adjust controller client

--- src/pseudoControllers/ClientController.js
+++ src/pseudoControllers/ClientController.js
@@ -1,29 +1,19 @@
-import fs from 'node:fs/promises';
-import path from 'path';
+import Clients from '../data/clients.json'
 
-const CLIENT_FILE = path.join(process.cwd(), 'src', 'data', 'clients.json');
-
-export const createClient = async () => {
-    // Implementa la lógica para crear un cliente
+export const createClient =  () => {
     console.log("Crear cliente");
-    // TODO: Implementar la lógica real
 }
 
-export const readClient = async () => {
-    // Implementa la lógica para leer un cliente
+export const readClient = () => {
     console.log("Leer cliente");
-    // TODO: Implementar la lógica real
+    
 }
 
-export const updateClient = async () => {
-    // Implementa la lógica para actualizar un cliente
+export const updateClient =  () => {
     console.log("Actualizar cliente");
-    // TODO: Implementar la lógica real
 }
 
-export const deleteClient = async () => {
-    // Implementa la lógica para eliminar un cliente
+export const deleteClient =  () => {
     console.log("Eliminar cliente");
-    // TODO: Implementar la lógica real
 }
 
